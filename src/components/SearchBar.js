49995a46dd05f9635dd1ad3499af0df62c8c8520
Eliza Plowden <elizaplowden@gmail.com>
2021-05-24 12:50:33 +0100

    refactored to class based component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  // onInputChange(event){
+  //   console.log(event.target.value);
+  // }
+  state = { term: ''};
+
+  onFormSubmit = event => {
+    // prevents form automatically submitting
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form
+          // now we're defining an arrow function and passing it down into form
+          // form will only invoke the arrow function one time
+          onSubmit={this.onFormSubmit}
+          className="ui form">
           <div className="field">
             <label>Country Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+              // onChange={this.onInputChange}
+              // arrow function apporoach - use when we have a single line of code to execute when callback is executed
+              // onChange={(e) => console.log(e.target.value)}
+            />
           </div>
         </form>
       </div>
